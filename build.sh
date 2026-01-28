bcff013335aa321cd767e7457e410b2194f96d10
Recipe Reco User <user@recipereco.local>
2026-01-29 00:30:39 +0800

    Fix line endings for build.sh and add build logging

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
+echo "Starting Build Process..."
 pip install -r requirements.txt
 
+echo "Collecting Static Files..."
 python manage.py collectstatic --no-input
+
+echo "Running Database Migrations..."
 python manage.py migrate
+
+echo "Build Process Complete!"
